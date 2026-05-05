c1fad0ff78972357eef18588fcf7f2c42dcc177d
sofiisunreal <sophiekendi17@gmail.com>
2026-05-05 19:43:55 +0300

    extra stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,46 +2,78 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    
+
     <section class="main-footer row bg-dark p-3 mt-8">
         {/* <!-- child 1 --> */}
         <div class="col-md-4 text-center text-white">
-            <h2>About Us</h2>
+            <h3>About Us</h3>
             <p className='text-start'>
-                We are one of the best selling online stores nationwide. We are mostly retail but offer wholesale.
-                To get the wholesale, please visit our offices or contact us.
+             We’re a leading online store offering quality products at great prices.
+            Retail & wholesale available.
             </p>
 
         </div>
 
         {/* <!-- child 2 --> */}
-        <div class="col-md-4">
-            <h2 class="text-center text-white">Contact Us</h2>
-            <form action="">
-                <input type="email" placeholder="Enter your email" class="form-control"/> <br/> <br/>
-                <textarea name="" id="" class="form-control" placeholder="Leave a comment"></textarea> <br/>
-                <input type="submit" value="Send message" class="btn btn-outline-danger"/>
-            </form>
-        </div>
+        <div class="col-md-4 text-white">
+            <div className="footer-section contact">
+              <h3>Contact Us</h3>
+              <div className="contact-item">
+                <span>📧</span>
+                <div>
+                  <p>Email</p>
 
+                  <small>
+                    <a href="mailto:support@yourstore.com">support@playquest.com</a>
+                    </small>
+                </div>
+
+
+
+              </div>
+              <div className="contact-item">
+                <span>📞</span>
+
+                <div>
+                  <p>Phone</p>
+                  <a href="tel:+254712345678">+254 712 345 678</a>
+                </div>
+              </div>
+              <div className="contact-item">
+                <span>📍</span>
+
+                <div>
+                  <p>Location</p>
+                  <small>Nairobi, Kenya</small>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+        </div>
         {/* <!-- child 3 --> */}
         <div class="col-md-4">
-            <h2 class="text-white text-center">Stay connected</h2>
-            <a href="https://facebook.com">
-                <img src="images/fb.png" alt="Facebook"/>
-            </a>
-            <a href="https://instagram.com">
-                <img src="images/.png" alt=""/>
-            </a>
-            <a href="https://twitter.com">
-                <img src="images/x.png" alt=""/>
-            </a>
-            <p className='text-white text-end'>In these platforms we have employed people who will be able to attend to you 24/7.Also follow us in
-                order not to miss out on deals!! </p>
+            <h3 class="text-white text-center">Stay connected</h3>
+            <div className='social-icons'>
+
+                <a href="https://facebook.com">
+                    <img src="images/fb.png" alt="Facebook"/>
+                </a>
+                <a href="https://instagram.com">
+                    <img src="images/in.png" alt=""/>
+                </a>
+                <a href="https://twitter.com">
+                    <img src="images/x.png" alt=""/>
+                </a> <br/> <br/>
+            </div> <br/>
+            <p className='text-white text-start'> Follow us for updates and exclusive deals </p>
         </div>
     </section>
 
   )
 }
 
-export default Footer 
+export default Footer
